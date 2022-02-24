060e22350bff7776c39ce6062406a52b3e548998
Joshua Liu <supajoshjunk@gmail.com>
2022-02-24 15:39:30 +0000

    search bar

--- data.js
+++ data.js
@@ -1,16 +1,5 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri = 'mongodb+srv://dev:opQn8C4dd7WQjajP@carcluster.u5h1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db('cars');
-    const col = database.collection('cars-all');
-    await col.createIndex({make: 'text', model: 'text', category: "string"});
-    console.log('done')
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const data = require('./data.json');
+const fs = require('fs');
+
+const newData = data.map(item => ({ name: `${ item.make } ${ item.model } ${ item.year }`, ...item }));
+fs.writeFileSync('./data.json', JSON.stringify(newData, null, 2));
--- pages/api/search.js
+++ pages/api/search.js
@@ -1,20 +1,22 @@
 import { connectToDatabase } from '/lib/mongodb.js';
 
 export default async function search(req, res) {
-  const { db } = await connectToDatabase();
-  const col = await db.collection('cars-all');
-  const data = await col.aggregate([
-    {
-      '$search': {
-        'index': 'default',
-        'text': {
-          'query': req.body.query,
-          'path': {
-            'wildcard': '*'
-          }
-        }
-      }
-    }
-  ]).limit(req.body.limit ? parseInt(req.body.limit) : 2 ** 31-1).toArray();
+	const { db } = await connectToDatabase();
+	const col = await db.collection('cars-all');
+	const data = await col.aggregate([
+		{
+			"$search": {
+        "index": "autocomplete"
+				"autocomplete": {
+					"query": `${req.body.query}`,
+					"path": "name",
+					"fuzzy": {
+						"maxEdits": 2,
+						"prefixLength": 3
+					}
+				}
+			}
+		}
+  ]).limit(req.body.limit ? parseInt(req.body.limit) : 2 ** 31 - 1).toArray();
   res.status(200).json(data);
 }
